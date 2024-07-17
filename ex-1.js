df5e1e2e00548cad80b00b6076ab2bc04d0fac95
Puttapong <puttapong.lekk@gmail.com>
2024-07-17 14:41:04 +0700

    add ex1-ex2

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,34 @@
 function findStudentIndex(students, searchStudent) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  let left = 0;
+  let right = students.length - 1;
+
+  while (left <= right) {
+    let midValue = Math.floor((left + right) / 2);
+    if (students[midValue] === searchStudent) {
+      return midValue;
+    } else if (students[midValue] < searchStudent) {
+      left = midValue + 1;
+    } else {
+      right = midValue - 1;
+    }
+  }
+  return -1;
 }
 
 // ตอบคำถามตรงนี้จ้า
+let students = [
+  "Andrew",
+  "Bobby",
+  "Diana",
+  "George",
+  "Hannah",
+  "Isaac",
+  "Jasmine",
+  "John",
+];
+
+let searchStudent = "Hannah";
+
+let result = findStudentIndex(students, searchStudent);
+console.log(result);
